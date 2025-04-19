744e2b2e1a9740fce7a0d2e3c818c93696a5981d
yossef-seyam <github.com/yossef-seyam>
2025-04-19 16:59:21 +0200

    Add STD.h header file to LIB folder

---
+++ Drivers/LIB/STD.h
@@ -0,0 +1,25 @@
+#ifndef STD_H_
+#define STD_H_
+
+
+#define false 0
+#define true  1
+
+typedef unsigned char u8;
+typedef signed char s8;
+
+typedef unsigned short int  u16;
+typedef signed short int  s16;
+
+typedef unsigned long int u32;
+typedef signed long int s32;
+
+typedef unsigned long long u64;
+typedef signed long long s64;
+
+
+typedef float f32;
+typedef double f64;
+
+
+#endif /* STD_H_ */
