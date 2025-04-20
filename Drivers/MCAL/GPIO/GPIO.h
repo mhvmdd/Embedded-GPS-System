9e488c3fea3b3857b6a5a23f67c7585510d37773
yossef-seyam <github.com/yossef-seyam>
2025-04-20 06:29:46 +0200

    Add GPIO.h header file to MCAL folder

--- Drivers/MCAL/GPIO/GPIO.h
+++ Drivers/MCAL/GPIO/GPIO.h
@@ -1,8 +1,156 @@
-#ifndef GPIO_H
-#define GPIO_H
+#ifndef GPIO_H_
+#define	GPIO_H_
+
+#include "../LIB/BIT_MATH.h"
+#include "../LIB/STD.h"
+
+
+
+#define AHB	0
+#define APB 1
+/* choose system clock
+	AHB
+	APB
+*/
+#define clk_type	AHB
+
+/*
+typedef struct{
+
+	volatile u32 DATA;	//0x000
+	volatile u32 DIR;		//0x400
+	volatile u32 AFSEL;	//0x420
+	volatile u32 PUR;		//0x510
+	volatile u32 PDR;		//0x514
+	volatile u32 DEN;		//0x51C
+	volatile u32 LOCK;	//0x520
+	volatile u32 CR;		//0x524
+	volatile u32 AMSEL;	//0x528
+	volatile u32 PCTL;	//0x52C
+
+}GPIO_Type;
+*/
+/*
+#define PORT_A ((volatile GPIO_Type*)0x40004000)
+
+#define PORT_B ((volatile GPIO_Type*)0x40005000)
+
+#define PORT_C ((volatile GPIO_Type*)0x40006000)
+
+#define PORT_D `((volatile GPIO_Type*)0x40007000)
+
+#define PORT_E `((volatile GPIO_Type*)0x40024000)
+
+#define PORT_F `((volatile GPIO_Type*)0x40025000)
+*/
+#if 	clk_type == APB
+#define GPIOA_Base_Address        0x40004000            // define the boundary address of GPIO port A
+#define GPIOB_Base_Address        0x40005000            // define the boundary address of GPIO port B
+#define GPIOC_Base_Address        0x40006000            // define the boundary address of GPIO port C
+#define GPIOD_Base_Address        0x40007000            // define the boundary address of GPIO port D
+#define GPIOE_Base_Address        0x40024000            // define the boundary address of GPIO port E
+#define GPIOF_Base_Address        0x40025000            // define the boundary address of GPIO port F
+#elif clk_type == AHB
+#define GPIOA_Base_Address        0x40058000            // define the boundary address of GPIO port A
+#define GPIOB_Base_Address        0x40059000            // define the boundary address of GPIO port B
+#define GPIOC_Base_Address        0x4005A000            // define the boundary address of GPIO port C
+#define GPIOD_Base_Address        0x4005B000            // define the boundary address of GPIO port D
+#define GPIOE_Base_Address        0x4005C000            // define the boundary address of GPIO port E
+#define GPIOF_Base_Address        0x4005D000            // define the boundary address of GPIO port F
+#endif
+/*PORT A Registers*/// 8 pins  //PIN0 & PIN1 are used for UART0
+#define GPIOA_DATA	      *((u32*)(GPIOA_Base_Address + 0x000))
+#define GPIOA_DIR         *((u32*)(GPIOA_Base_Address + 0x400))
+#define GPIOA_AFSEL	      *((u32*)(GPIOA_Base_Address + 0x420))
+#define GPIOA_PUR         *((u32*)(GPIOA_Base_Address + 0x510))
+#define GPIOA_PDR	        *((u32*)(GPIOA_Base_Address + 0x514))
+#define GPIOA_DEN	        *((u32*)(GPIOA_Base_Address + 0x51C))
+#define GPIOA_LOCK        *((u32*)(GPIOA_Base_Address + 0x520))
+#define GPIOA_CR					*((u32*)(GPIOA_Base_Address + 0x524))
+#define GPIOA_AMSEL				*((u32*)(GPIOA_Base_Address + 0x528))
+#define GPIOA_PCTL				*((u32*)(GPIOA_Base_Address + 0x52C))
+
+/*PORT B Registers*/// 8 pins
+
+
+/*PORT C Registers*/// 8 pins
+
+
+/*PORT D Registers*/// 8 pins
+
+
+/*PORT E Registers*/// 6 pins
+
+
+/*PORT F Registers*/// 5 pins
+#define GPIOF_DATA	      *((u32*)(GPIOF_Base_Address + 0x000))
+#define GPIOF_DIR         *((u32*)(GPIOF_Base_Address + 0x400))
+#define GPIOF_AFSEL	      *((u32*)(GPIOF_Base_Address + 0x420))
+#define GPIOF_PUR         *((u32*)(GPIOF_Base_Address + 0x510))
+#define GPIOF_PDR	        *((u32*)(GPIOF_Base_Address + 0x514))
+#define GPIOF_DEN	        *((u32*)(GPIOF_Base_Address + 0x51C))
+#define GPIOF_LOCK        *((u32*)(GPIOF_Base_Address + 0x520))
+#define GPIOF_CR					*((u32*)(GPIOF_Base_Address + 0x524))
+#define GPIOF_AMSEL				*((u32*)(GPIOF_Base_Address + 0x528))
+#define GPIOF_PCTL				*((u32*)(GPIOF_Base_Address + 0x52C))
+
+/*Define Ports*/
+#define PORTA	0
+#define PORTB	1
+#define PORTC	2
+#define PORTD	3
+#define PORTE	4
+#define PORTF	5
+
+/*Define Pins*/
+#define PIN0	0
+#define PIN1	1
+#define PIN2	2
+#define PIN3	3
+#define PIN4	4
+#define PIN5	5
+#define PIN6	6
+#define PIN7	7
+
+#define HIGH	1
+#define LOW		0
+
+#define OUTPUT 1
+#define INPUT	 0
+
+/*Functions*/
+
+void DIO_voidSetPinDirection(char port,char pin, char direction);
+
+void DIO_voidSetPinValue(char port,char pin, char value);
+
+char DIO_u8GetPinValue(char port,char pin);
+
+void DIO_voidSetPortDirection(char port,char direction);
+
+void DIO_voidSetPortValue(char port,char value);
+
+void DIO_VoidTogglePin(char port,char pin);
+
+char DIO_u8GetPortValue(char port,char pin);
+
+
+
+
+#endif
 
 
 
 
 
-#endif /*GPIO_H*/
+
+
+
+
+
+
+
+
+
+
+
