eecf7e4a63f2c15f26adb766fe7361dfe787ee9a
M0hamed-Mahmoud <mohamedmody1552004@gmail.com>
2025-04-23 00:20:44 +0200

    updated BIT_MATH.h

--- Drivers/LIB/BIT_MATH.h
+++ Drivers/LIB/BIT_MATH.h
@@ -5,6 +5,10 @@
 #define CLR_BIT(REG,BIT)          REG &= ~(1 << (BIT))	//clear bit
 #define GET_BIT(REG,BIT)          ((REG >> BIT) & 1  )	//get bit
 #define TOG_BIT(REG,BIT)          REG ^=  (1 << (BIT))  //toggle bit
+#define SET(REG,VAL)              REG|= VAL             //set more than 1 bit
+#define CLR(REG,VAL)              REG&=~ VAL            //clear more than 1 bit
+#define GET(REG)                  (REG&0Xff)             // get more than 1 bit
+#define CHECK_REG(REG1,REG2)       (REG1&REG2)
 
 
-#endif 
+#endif
