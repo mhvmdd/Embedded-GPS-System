7faeac6d9298ac07efa98bc8a24a674081314e56
yossef-seyam <yoyoseyam12345@gmail.com>
2025-04-23 22:05:17 +0200

    added Port functions definitions

--- Drivers/MCAL/GPIO/GPIO.h
+++ Drivers/MCAL/GPIO/GPIO.h
@@ -2,7 +2,6 @@
 #define	GPIO_H_
 
 #include "BIT_MATH.h" 
-
 #include "STD.h" 
 
 
@@ -38,76 +37,76 @@
 
 #endif
 /*PORT A Registers*/// 8 pins  //PIN0 & PIN1 are used for UART0
-#define GPIO_PORTA_DATA	       *((volatile u32*)(GPIO_PORTA_Base_Address + 0x3FC))
-#define GPIO_PORTA_DIR         *((volatile u32*)(GPIO_PORTA_Base_Address + 0x400))
-#define GPIO_PORTA_AFSEL	     *((volatile u32*)(GPIO_PORTA_Base_Address + 0x420))
-#define GPIO_PORTA_PUR         *((volatile u32*)(GPIO_PORTA_Base_Address + 0x510))
-#define GPIO_PORTA_PDR	       *((volatile u32*)(GPIO_PORTA_Base_Address + 0x514))
-#define GPIO_PORTA_DEN	       *((volatile u32*)(GPIO_PORTA_Base_Address + 0x51C))
-#define GPIO_PORTA_LOCK        *((volatile u32*)(GPIO_PORTA_Base_Address + 0x520))
-#define GPIO_PORTA_CR					 *((volatile u32*)(GPIO_PORTA_Base_Address + 0x524))
-#define GPIO_PORTA_AMSEL			 *((volatile u32*)(GPIO_PORTA_Base_Address + 0x528))
-#define GPIO_PORTA_PCTL				 *((volatile u32*)(GPIO_PORTA_Base_Address + 0x52C))
+#define GPIO_PORTA_DATA	       	*((volatile u32*)(GPIO_PORTA_Base_Address + 0x3FC))
+#define GPIO_PORTA_DIR         	*((volatile u32*)(GPIO_PORTA_Base_Address + 0x400))
+#define GPIO_PORTA_AFSEL	    *((volatile u32*)(GPIO_PORTA_Base_Address + 0x420))
+#define GPIO_PORTA_PUR         	*((volatile u32*)(GPIO_PORTA_Base_Address + 0x510))
+#define GPIO_PORTA_PDR	       	*((volatile u32*)(GPIO_PORTA_Base_Address + 0x514))
+#define GPIO_PORTA_DEN	       	*((volatile u32*)(GPIO_PORTA_Base_Address + 0x51C))
+#define GPIO_PORTA_LOCK        	*((volatile u32*)(GPIO_PORTA_Base_Address + 0x520))
+#define GPIO_PORTA_CR			*((volatile u32*)(GPIO_PORTA_Base_Address + 0x524))
+#define GPIO_PORTA_AMSEL		*((volatile u32*)(GPIO_PORTA_Base_Address + 0x528))
+#define GPIO_PORTA_PCTL			*((volatile u32*)(GPIO_PORTA_Base_Address + 0x52C))
 
 /*PORT B Registers*/// 8 pins 
-#define GPIO_PORTB_DATA	       *((volatile u32*)(GPIO_PORTB_Base_Address + 0x3FC))
-#define GPIO_PORTB_DIR         *((volatile u32*)(GPIO_PORTB_Base_Address + 0x400))
-#define GPIO_PORTB_AFSEL	     *((volatile u32*)(GPIO_PORTB_Base_Address + 0x420))
-#define GPIO_PORTB_PUR         *((volatile u32*)(GPIO_PORTB_Base_Address + 0x510))
-#define GPIO_PORTB_PDR	       *((volatile u32*)(GPIO_PORTB_Base_Address + 0x514))
-#define GPIO_PORTB_DEN	       *((volatile u32*)(GPIO_PORTB_Base_Address + 0x51C))
-#define GPIO_PORTB_LOCK        *((volatile u32*)(GPIO_PORTB_Base_Address + 0x520))
-#define GPIO_PORTB_CR				   *((volatile u32*)(GPIO_PORTB_Base_Address + 0x524))
-#define GPIO_PORTB_AMSEL			 *((volatile u32*)(GPIO_PORTB_Base_Address + 0x528))
-#define GPIO_PORTB_PCTL			   *((volatile u32*)(GPIO_PORTB_Base_Address + 0x52C))
+#define GPIO_PORTB_DATA			*((volatile u32*)(GPIO_PORTB_Base_Address + 0x3FC))
+#define GPIO_PORTB_DIR			*((volatile u32*)(GPIO_PORTB_Base_Address + 0x400))
+#define GPIO_PORTB_AFSEL		*((volatile u32*)(GPIO_PORTB_Base_Address + 0x420))
+#define GPIO_PORTB_PUR			*((volatile u32*)(GPIO_PORTB_Base_Address + 0x510))
+#define GPIO_PORTB_PDR			*((volatile u32*)(GPIO_PORTB_Base_Address + 0x514))
+#define GPIO_PORTB_DEN			*((volatile u32*)(GPIO_PORTB_Base_Address + 0x51C))
+#define GPIO_PORTB_LOCK			*((volatile u32*)(GPIO_PORTB_Base_Address + 0x520))
+#define GPIO_PORTB_CR			*((volatile u32*)(GPIO_PORTB_Base_Address + 0x524))
+#define GPIO_PORTB_AMSEL		*((volatile u32*)(GPIO_PORTB_Base_Address + 0x528))
+#define GPIO_PORTB_PCTL			*((volatile u32*)(GPIO_PORTB_Base_Address + 0x52C))
 
 /*PORT C Registers*/// 8 pins 
 #define GPIO_PORTC_DATA	        *((volatile u32*)(GPIO_PORTC_Base_Address + 0x3FC))
 #define GPIO_PORTC_DIR          *((volatile u32*)(GPIO_PORTC_Base_Address + 0x400))
-#define GPIO_PORTC_AFSEL	      *((volatile u32*)(GPIO_PORTC_Base_Address + 0x420))
+#define GPIO_PORTC_AFSEL	    *((volatile u32*)(GPIO_PORTC_Base_Address + 0x420))
 #define GPIO_PORTC_PUR          *((volatile u32*)(GPIO_PORTC_Base_Address + 0x510))
 #define GPIO_PORTC_PDR	        *((volatile u32*)(GPIO_PORTC_Base_Address + 0x514))
 #define GPIO_PORTC_DEN	        *((volatile u32*)(GPIO_PORTC_Base_Address + 0x51C))
 #define GPIO_PORTC_LOCK         *((volatile u32*)(GPIO_PORTC_Base_Address + 0x520))
-#define GPIO_PORTC_CR					  *((volatile u32*)(GPIO_PORTC_Base_Address + 0x524))
-#define GPIO_PORTC_AMSEL				*((volatile u32*)(GPIO_PORTC_Base_Address + 0x528))
-#define GPIO_PORTC_PCTL				  *((volatile u32*)(GPIO_PORTC_Base_Address + 0x52C))
+#define GPIO_PORTC_CR			*((volatile u32*)(GPIO_PORTC_Base_Address + 0x524))
+#define GPIO_PORTC_AMSEL		*((volatile u32*)(GPIO_PORTC_Base_Address + 0x528))
+#define GPIO_PORTC_PCTL			*((volatile u32*)(GPIO_PORTC_Base_Address + 0x52C))
 
 /*PORT D Registers*/// 8 pins 
 #define GPIO_PORTD_DATA	        *((volatile u32*)(GPIO_PORTD_Base_Address + 0x3FC))
 #define GPIO_PORTD_DIR          *((volatile u32*)(GPIO_PORTD_Base_Address + 0x400))
-#define GPIO_PORTD_AFSEL	      *((volatile u32*)(GPIO_PORTD_Base_Address + 0x420))
+#define GPIO_PORTD_AFSEL		*((volatile u32*)(GPIO_PORTD_Base_Address + 0x420))
 #define GPIO_PORTD_PUR          *((volatile u32*)(GPIO_PORTD_Base_Address + 0x510))
 #define GPIO_PORTD_PDR	        *((volatile u32*)(GPIO_PORTD_Base_Address + 0x514))
 #define GPIO_PORTD_DEN	        *((volatile u32*)(GPIO_PORTD_Base_Address + 0x51C))
 #define GPIO_PORTD_LOCK         *((volatile u32*)(GPIO_PORTD_Base_Address + 0x520))
-#define GPIO_PORTD_CR					  *((volatile u32*)(GPIO_PORTD_Base_Address + 0x524))
-#define GPIO_PORTD_AMSEL				*((volatile u32*)(GPIO_PORTD_Base_Address + 0x528))
-#define GPIO_PORTD_PCTL				  *((volatile u32*)(GPIO_PORTD_Base_Address + 0x52C))
+#define GPIO_PORTD_CR			*((volatile u32*)(GPIO_PORTD_Base_Address + 0x524))
+#define GPIO_PORTD_AMSEL		*((volatile u32*)(GPIO_PORTD_Base_Address + 0x528))
+#define GPIO_PORTD_PCTL			*((volatile u32*)(GPIO_PORTD_Base_Address + 0x52C))
 
 /*PORT E Registers*/// 6 pins 
 #define GPIO_PORTE_DATA	        *((volatile u32*)(GPIO_PORTE_Base_Address + 0x3FC))
 #define GPIO_PORTE_DIR          *((volatile u32*)(GPIO_PORTE_Base_Address + 0x400))
-#define GPIO_PORTE_AFSEL	      *((volatile u32*)(GPIO_PORTE_Base_Address + 0x420))
+#define GPIO_PORTE_AFSEL	    *((volatile u32*)(GPIO_PORTE_Base_Address + 0x420))
 #define GPIO_PORTE_PUR          *((volatile u32*)(GPIO_PORTE_Base_Address + 0x510))
 #define GPIO_PORTE_PDR	        *((volatile u32*)(GPIO_PORTE_Base_Address + 0x514))
 #define GPIO_PORTE_DEN	        *((volatile u32*)(GPIO_PORTE_Base_Address + 0x51C))
 #define GPIO_PORTE_LOCK         *((volatile u32*)(GPIO_PORTE_Base_Address + 0x520))
-#define GPIO_PORTE_CR					  *((volatile u32*)(GPIO_PORTE_Base_Address + 0x524))
-#define GPIO_PORTE_AMSEL			  *((volatile u32*)(GPIO_PORTE_Base_Address + 0x528))
-#define GPIO_PORTE_PCTL				  *((volatile u32*)(GPIO_PORTE_Base_Address + 0x52C))
+#define GPIO_PORTE_CR			*((volatile u32*)(GPIO_PORTE_Base_Address + 0x524))
+#define GPIO_PORTE_AMSEL		*((volatile u32*)(GPIO_PORTE_Base_Address + 0x528))
+#define GPIO_PORTE_PCTL			*((volatile u32*)(GPIO_PORTE_Base_Address + 0x52C))
 
 /*PORT F Registers*/// 5 pins 
-#define GPIO_PORTF_DATA	        *((volatile u32*)(GPIO_PORTF_Base_Address + 0x3FC))
+#define GPIO_PORTF_DATA			*((volatile u32*)(GPIO_PORTF_Base_Address + 0x3FC))
 #define GPIO_PORTF_DIR          *((volatile u32*)(GPIO_PORTF_Base_Address + 0x400))
-#define GPIO_PORTF_AFSEL	      *((volatile u32*)(GPIO_PORTF_Base_Address + 0x420))
+#define GPIO_PORTF_AFSEL	    *((volatile u32*)(GPIO_PORTF_Base_Address + 0x420))
 #define GPIO_PORTF_PUR          *((volatile u32*)(GPIO_PORTF_Base_Address + 0x510))
 #define GPIO_PORTF_PDR	        *((volatile u32*)(GPIO_PORTF_Base_Address + 0x514))
 #define GPIO_PORTF_DEN	        *((volatile u32*)(GPIO_PORTF_Base_Address + 0x51C))
 #define GPIO_PORTF_LOCK         *((volatile u32*)(GPIO_PORTF_Base_Address + 0x520))
-#define GPIO_PORTF_CR					  *((volatile u32*)(GPIO_PORTF_Base_Address + 0x524))
-#define GPIO_PORTF_AMSEL				*((volatile u32*)(GPIO_PORTF_Base_Address + 0x528))
-#define GPIO_PORTF_PCTL				  *((volatile u32*)(GPIO_PORTF_Base_Address + 0x52C))
+#define GPIO_PORTF_CR			*((volatile u32*)(GPIO_PORTF_Base_Address + 0x524))
+#define GPIO_PORTF_AMSEL		*((volatile u32*)(GPIO_PORTF_Base_Address + 0x528))
+#define GPIO_PORTF_PCTL			*((volatile u32*)(GPIO_PORTF_Base_Address + 0x52C))
 
 /*Define Ports*/
 #define PORTA	0
@@ -129,11 +128,15 @@
 
 #define HIGH	1
 #define LOW		0
+#define PORT_HIGH	0xFF
+#define PORT_LOW	0x00
 //for digital
 
-#define OUTPUT 1
-#define INPUT	 0
+#define OUTPUT	1
+#define INPUT	0
 
+#define PORT_OUTPUT	0xFF
+#define PORT_INPUT	0x00
 // GPIO LOCK KEY
 #define GPIO_LOCK_KEY 0x4C4F434B
 
