94552af1c1040326b5d45e3f51f596e45b6c6efb
Doha Mohamed <dohahabeb15@gmail.com>
2025-04-24 02:38:25 +0200

    add EEPROM File

---
+++ Embedded-Project/eeprom.d
@@ -0,0 +1,4 @@
+.\eeprom.o: EEPROM.c
+.\eeprom.o: BIT_MATH.h
+.\eeprom.o: STD.h
+.\eeprom.o: tm4c123gh6pm.h
---
+++ Embedded-Project/main.d
@@ -0,0 +1,5 @@
+.\main.o: main.c
+.\main.o: BIT_MATH.h
+.\main.o: STD.h
+.\main.o: tm4c123gh6pm.h
+.\main.o: EEPROM.h
