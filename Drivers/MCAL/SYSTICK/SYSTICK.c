994b8604c6f5c6c714b7386acfcfb9ea803153d2
Mohammed Yasser <mohammeduni585@gmail.com>
2025-05-05 23:35:22 +0300

    Added the SYSTICK Driver

--- Drivers/MCAL/SYSTICK/SYSTICK.c
+++ Drivers/MCAL/SYSTICK/SYSTICK.c
@@ -1,5 +1,37 @@
 
 
 #include "SYSTICK.H"
+#include "../../../Embedded-Project/TM4C123.h"
 
+static void (*SysTick_Callback)(void) = 0;
 
+void SYSTICK_Init(void) {
+    NVIC_ST_CTRL_R = 0;                    // Disable SysTick during setup
+    NVIC_ST_RELOAD_R = 0x00FFFFFF;         // Maximum reload value
+    NVIC_ST_CURRENT_R = 0;                 // Clear the current value register
+    NVIC_ST_CTRL_R = 0x07;                 // Enable SysTick with core clock and interrupt
+}
+
+void SYSTICK_DelayMs(unsigned int delay) {
+		unsigned int i;
+	
+    NVIC_ST_CTRL_R = 0; // Disable SysTick during setup
+    NVIC_ST_RELOAD_R = 16000 - 1; // 1ms delay at 16 MHz
+    NVIC_ST_CURRENT_R = 0; // Clear current value
+    NVIC_ST_CTRL_R = 0x5; // Enable SysTick with core clock, no interrupt
+		
+    for(i = 0; i < delay; i++) {
+        while((NVIC_ST_CTRL_R & 0x00010000) == 0); // Wait for COUNT flag
+    }
+
+    NVIC_ST_CTRL_R = 0; // Turn off SysTick after delay
+}
+void SYSTICK_SetCallback(void (*callback)(void)) {
+    SysTick_Callback = callback;
+}
+
+void SYSTICK_Handler(void) {
+    if (SysTick_Callback) {
+        SysTick_Callback();  // Call the user-defined function
+    }
+}
--- Drivers/MCAL/SYSTICK/SYSTICK.h
+++ Drivers/MCAL/SYSTICK/SYSTICK.h
@@ -2,7 +2,10 @@
 #define SYSTICK_H
 
 
-
+void SYSTICK_Init(void);
+void SYSTICK_DelayMs(unsigned int delay);
+void SYSTICK_SetCallback(void (*callback)(void));
+void SYSTICK_Handler(void);
 
 
 #endif /*SYSTICK_H*/
