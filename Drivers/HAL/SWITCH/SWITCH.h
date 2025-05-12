4a641985b0509a314b863fc6b565d434d4f0d325
M0hamed-Mahmoud <mohamedmody1552004@gmail.com>
2025-05-12 23:21:43 +0300

    switch.h updated

--- Drivers/HAL/SWITCH/SWITCH.h
+++ Drivers/HAL/SWITCH/SWITCH.h
@@ -1,8 +1,14 @@
+
+ /*
+ * SWITCH.h
+ *
+ *  Created on: May 10, 2025
+ *      Author: Mohammed Mahmoud
+ */
+
 #ifndef SWITCH_H
 #define SWITCH_H
 
 void HAL_SWsInit();
-void HAL_SWsSetCallback(void (*callback)(void));
-void HAL_SWsInterruptInit();
 
 #endif
