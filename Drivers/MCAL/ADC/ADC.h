50cfa33751857593d409d1b3232b9cf01bc1d150
nadine214 <nadineamr214@gmail.com>
2025-05-13 03:13:19 +0300

    ADC Driver

---
+++ Drivers/MCAL/ADC/ADC.h
@@ -0,0 +1,19 @@
+
+ /*
+ * ADC.h
+ *
+ *  Created on: May 10, 2025
+ *      Author: Nadine Amr
+ */
+
+#ifndef ADC_H
+#define ADC_H
+
+
+
+#include <stdint.h>
+
+void ADC_Init(void);
+uint32_t ADC_Read(void);
+
+#endif
